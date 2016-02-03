c16198ec77b8c6baf4c06613f784f8bea6ca51a5
splacorn <curtischong5@gmail.com>
2016-02-03 13:14:17 -0500

    added more exec photo rows

--- our_executive_team/our-executive-team.php
+++ our_executive_team/our-executive-team.php
@@ -56,6 +56,14 @@
                 <div class="col-sm-3">
                 </div>
             </div>
+            <div class="row-fluid">
+                <div class="col-sm-3">
+                </div>
+                <div class="col-sm-3">
+                </div>
+                <div class="col-sm-3">
+                </div>
+            </div>
         </div>
     </div>
        <?php include '../footer-folders.php'; ?>
